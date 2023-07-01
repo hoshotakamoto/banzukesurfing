8779c282870752c16d9559168b8cd5ec510463a2
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-01 11:32:01 -0700

    Create game.js

---
+++ game.js
@@ -0,0 +1,7 @@
+function startPlaying() {
+    var rikishi = $('#rikishi').val();
+    // This is where you'd connect to your game logic
+    // For example:
+    // sendRikishiToServer(rikishi);
+    alert("You selected: " + rikishi);
+}
