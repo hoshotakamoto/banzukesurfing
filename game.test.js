cbddb6ebdf03ad8c40e35da4125c9156869aef11
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-02 23:45:33 -0700

    Update game.test.js

--- game.test.js
+++ game.test.js
@@ -1,5 +1,10 @@
+global.$ = jest.fn(() => ({
+    val: jest.fn(() => '1')
+}));
+
 const { startPlaying } = require('./game');
 
-test('check if startPlaying is defined', () => {
-  expect(startPlaying).toBeDefined();
+test('check if startPlaying is defined and returns expected value', () => {
+    const result = startPlaying()
+    expect(result).toBe("You selected: 1");
 });
