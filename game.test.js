6362a1e8f24495c8e63bbb9d42e214f9a8451054
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-02 23:59:22 -0700

    Update game.test.js

--- game.test.js
+++ game.test.js
@@ -1,10 +1,18 @@
+require('jest-localstorage-mock'); // import the local storage mock
+
 global.$ = jest.fn(() => ({
     val: jest.fn(() => '1')
 }));
 
 const { startPlaying } = require('./game');
 
+beforeEach(() => {
+    localStorage.clear(); // clear localStorage before each test
+});
+
 test('check if startPlaying is defined and returns expected value', () => {
-    const result = startPlaying()
-    expect(result).toBe("You selected: 1");
+    localStorage.setItem('user', 'testUser');
+    localStorage.setItem('testUser', JSON.stringify({ 'July 2023': '1' })); // Assume a previous pick
+    const result = startPlaying();
+    expect(result).toBe("You selected: 1\nPrevious Picks: {\"July 2023\":\"1\"}");
 });
