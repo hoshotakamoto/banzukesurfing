7cc0408af7898de48e68e6ceb20eb46f92213252
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-03 13:15:16 -0700

    Update game.test.js

--- game.test.js
+++ game.test.js
@@ -15,9 +15,12 @@ beforeEach(() => {
             <option value="1">Rikishi 1</option>
             <option value="2">Rikishi 2</option>
         </select>
+        <button id="startPlayingButton">Start Playing</button>
+        <button id="switchUserButton">Switch User</button>
         <input id="userSwitch" type="text">
         <input id="backfillContest" type="text">
         <input id="backfillRikishi" type="text">
+        <button id="backfillResultsButton">Backfill Results</button>
         <div id="backfilledResults"></div>
         <p id="feedback"></p>
     `;
