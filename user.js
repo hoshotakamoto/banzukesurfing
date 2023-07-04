8fa47afe98edea1e44b2fcecca147dd941e51f20
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-04 16:36:36 -0700

    Update user.js

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 export default class User {
-    constructor(name) {
+    constructor(name, data = {}) {
         this.name = name || this.getStoredUser();
+        this.data = data;
     }
 
     getStoredUser() {
@@ -13,46 +14,32 @@ export default class User {
     }
 
     getPicks() {
-        const picks = JSON.parse(localStorage.getItem(this.name));
-        return picks || {};
+        return this.data;
     }
 
     updatePicks(rikishi) {
-        const picks = this.getPicks();
         const currentContest = new Date().getMonth();
         if ([0, 2, 4, 6, 8, 10].includes(currentContest)) {
             const contestName = new Date().toLocaleString('default', { month: 'long' }) + ' ' + new Date().getFullYear();
-            picks[contestName] = rikishi;
-            localStorage.setItem(this.name, JSON.stringify(picks));
+            this.data[contestName] = rikishi;
         }
     }
 
     backfillResults(contestName, rikishi) {
-        const picks = this.getPicks();
-        picks[contestName] = rikishi;
-        localStorage.setItem(this.name, JSON.stringify(picks));
+        this.data[contestName] = rikishi;
     }
 
     switchUser(newUser) {
         localStorage.setItem('user', newUser);
-        this.name = newUser;
     }
 
     displayBackfilledResults() {
-        const picks = this.getPicks();
-        const resultsElement = document.querySelector('#backfilledResults');
-
-        // Clear previous results
-        resultsElement.textContent = '';
-
-        // Display each contest result
+        let results = '';
         for (const contest in picks) {
-            const rikishi = picks[contest];
-            const resultText = document.createTextNode(contest + ': ' + rikishi);
-            const resultDiv = document.createElement('div');
-            resultDiv.appendChild(resultText);
-            resultsElement.appendChild(resultDiv);
+            const rikishi = this.data[contest];
+            results += `${contest}: ${rikishi}\n`;
         }
+        return results;
     }
 
     getUserDetails() {
