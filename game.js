42e1926436b50128cb62865eb69ea8970ed82127
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-04 15:30:47 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,76 +1,23 @@
+import User from './user.js';
+
 export default class Game {
     constructor() {
-        this.user = this.getUser();
+        this.user = new User();
     }
 
     startPlaying() {
         const rikishi = document.querySelector('#rikishi').value;
-        const picks = this.getPicks();
+        const picks = this.user.getPicks();
         const message = "You selected: " + rikishi + "\nPrevious Picks: " + JSON.stringify(picks);
-        this.updatePicks(rikishi); // Update the picks with the new selection
+        this.user.updatePicks(rikishi); // Update the picks with the new selection
         return message;
-    }
-
-    getUser() {
-        // get user from local storage
-        let user = localStorage.getItem('user');
-        if (!user) {
-            user = 'admin';
-            localStorage.setItem('user', user);
-        }
-        return user;
-    }
-
-    getPicks() {
-        const picks = JSON.parse(localStorage.getItem(this.user));
-        if (!picks) {
-            return {};
-        }
-        return picks;
-    }
-
-    updatePicks(rikishi) {
-        const picks = this.getPicks();
-        const currentContest = new Date().getMonth();
-        if ([0, 2, 4, 6, 8, 10].includes(currentContest)) {
-            const contestName = new Date().toLocaleString('default', { month: 'long' }) + ' ' + new Date().getFullYear();
-            picks[contestName] = rikishi;
-            localStorage.setItem(this.user, JSON.stringify(picks));
-        }
-    }
-
-    switchUser() {
-        const newUser = document.querySelector('#userSwitch').value;
-        localStorage.setItem('user', newUser);
-        document.querySelector('#user').textContent = 'Current user: ' + newUser;
-        this.user = newUser;
     }
 
     backfillResults() {
         const contestName = document.querySelector('#backfillContest').value;
         const rikishi = document.querySelector('#backfillRikishi').value;
-        const picks = this.getPicks();
-        picks[contestName] = rikishi;
-        localStorage.setItem(this.user, JSON.stringify(picks));
-        this.provideFeedback('Backfilled results for ' + contestName + ' with ' + rikishi); // Provide feedback
-        this.displayBackfilledResults(); // Display the updated results
-    }
-
-    displayBackfilledResults() {
-        const picks = this.getPicks();
-        const resultsElement = document.querySelector('#backfilledResults');
-
-        // Clear previous results
-        resultsElement.textContent = '';
-
-        // Display each contest result
-        for (const contest in picks) {
-            const rikishi = picks[contest];
-            const resultText = document.createTextNode(contest + ': ' + rikishi);
-            const resultDiv = document.createElement('div');
-            resultDiv.appendChild(resultText);
-            resultsElement.appendChild(resultDiv);
-        }
+        this.user.backfillResults(contestName, rikishi);
+        this.user.displayBackfilledResults(); // Display the updated results
     }
 
     provideFeedback(message) {
@@ -78,15 +25,10 @@ export default class Game {
     }
 
     initialize() {
-        const userElement = document.querySelector('#user');
-        if (userElement) {
-            userElement.textContent = 'Current user: ' + this.user;
-        }
-        this.displayBackfilledResults(); // Display the initial results
+        this.user.initialize();
 
         // Add event listeners
         document.querySelector("#startPlayingButton").addEventListener('click', () => this.startPlaying());
-        document.querySelector("#switchUserButton").addEventListener('click', () => this.switchUser());
         document.querySelector("#backfillResultsButton").addEventListener('click', () => this.backfillResults());
     }
 }
