e24afe38fcf6ca996a8ed93114b4a4ff2b5d5e13
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-04 14:34:52 -0700

    Update game.test.js

--- game.test.js
+++ game.test.js
@@ -46,3 +46,36 @@ test('check if backfillResults updates the results in UI', () => {
     let resultsElement = document.querySelector('#backfilledResults');
     expect(resultsElement.textContent).toContain('May 2023: 2');
 });
+
+describe('Game', () => {
+    beforeEach(() => {
+        // Clear localStorage before each test
+        localStorage.clear();
+    });
+
+    it('should render localStorage on initialization', () => {
+        // Set something in localStorage
+        localStorage.setItem('test', 'testValue');
+
+        const game = new Game(true);
+
+        // Check if the initialize function has been called and content is rendered
+        // Here, it assumes that you have a way to check whether the content is rendered on the page
+        expect(game.initialize).toBeCalled();
+    });
+
+    it('should always call initialize on window load', () => {
+        // Define a mock function for the initialize method
+        const mockInitialize = jest.fn();
+
+        // Replace the original method with the mock function
+        Game.prototype.initialize = mockInitialize;
+
+        // Simulate window load
+        require('./game.js');
+
+        // Check if initialize has been called
+        expect(mockInitialize).toBeCalled();
+    });
+});
+
