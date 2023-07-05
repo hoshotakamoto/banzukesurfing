f4b1b653c621cb48b27066142842461930abb12e
hoshotakamoto <138322910+hoshotakamoto@users.noreply.github.com>
2023-07-05 10:51:21 -0700

    Create pick.js

---
+++ pick.js
@@ -0,0 +1,8 @@
+class Pick {
+  constructor(wrestlerName) {
+    this.wrestlerName = wrestlerName;
+    // You can add more properties relevant to the pick if needed
+  }
+}
+
+module.exports = Pick;
